086abfe50105bdaf253d7934529dbe89ff783ed1
pgm-tesssloc <tess.slock@student.arteveldehs.be>
2024-10-01 16:15:25 +0200

    made types for recipes & categories

--- rush_recipe_organiser/app/recipes/page.tsx
+++ rush_recipe_organiser/app/recipes/page.tsx
@@ -1,3 +1,20 @@
 export default function Page() {
+    type Recipe = {
+        id: number;
+        title: string;
+        ingredients: string[];
+        instructions: string;
+        category: Category;
+    }
 
+    type Category = 'ontbijt' | 'lunch' | 'diner' | 'snack';
+
+    return (
+        <div>
+            <h2>Recepten</h2>
+            <div>
+
+            </div>
+        </div>
+    )
 };
---
+++ rush_recipe_organiser/components/common/Recipe.tsx
@@ -0,0 +1,9 @@
+export default function Recipe() {
+    
+    return (
+        <div>
+            
+
+        </div>
+    )
+}
