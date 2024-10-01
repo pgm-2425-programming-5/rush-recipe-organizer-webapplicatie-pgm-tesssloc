431637ecdf39e85283c773dbfbb14d77d5afbfc7
pgm-tesssloc <tess.slock@student.arteveldehs.be>
2024-10-01 16:02:12 +0200

    added hero to home page

---
+++ rush_recipe_organiser/components/common/Link.tsx
@@ -0,0 +1,7 @@
+export default function Link () {
+    return (
+        <a href="/recipes" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Bekijk de recepten
+        </a>
+    )
+}
---
+++ rush_recipe_organiser/components/layout/Hero.tsx
@@ -0,0 +1,10 @@
+import Link from "../common/Link";
+
+export default function Hero () {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <p className="text-lg mt-2">Organiseer je favoriete recepten</p>
+            <Link />
+        </div>
+    )
+}
